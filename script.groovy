d87048e386959f8d2d74b99f731376742d0fac38
Smith Egharevba <57641192+BlaqSmyth@users.noreply.github.com>
2024-11-21 13:12:16 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,3 +1,17 @@
+def buildJar() {
+    echo 'building the application...'
+    sh 'mvn package'
+}
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t blaqsmyth/demo-app:jma-4.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
+        sh 'docker push blaqsmyth/demo-app:jma-4.0'
+    }
+}
+
 def deployApp() {
     echo 'deploying the application...'
 }
